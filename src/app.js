492696333c3b2c53eb52aba825ce5cc051f5acf3
Gustavo <gutavitus@gmail.com>
2025-11-07 18:02:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
+// Generador de excusas
 
+const who = ["El perro", "Mi abuela", "El cartero", "Mi profesor", "Mi loro"];
+const action = ["se comió", "mojó", "rompió", "perdió", "secuestró"];
+const what = ["mi tarea", "mi teléfono", "mi coche", "la presentación", "mi sandwich"];
+const when = ["antes de clase", "mientras dormía", "en el recreo", "durante la cena", "cuando llovía"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Devuelve un elemento aleatorio de un array
+function random(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Genera una excusa concatenando un elemento de cada array
+function generateExcuse() {
+  return `${random(who)} ${random(action)} ${random(what)} ${random(when)}.`;
+}
+
+// Cuando la página carga, genera una excusa y la muestra en el <p id="excuse">
+window.onload = () => {
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.innerText = generateExcuse();
+
+  // También genera una nueva al hacer clic en el botón
+  const newButton = document.getElementById("new");
+  newButton.onclick = () => {
+    excuseElement.innerText = generateExcuse();
+  };
 };
